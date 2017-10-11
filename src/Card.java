d1c2f6c901b6fc6093c11917922f59b9881c173e
dntf0llow <32690330+dntf0llow@users.noreply.github.com>
2017-10-11 18:11:21 +0200

    Working methods

--- src/Card.java
+++ src/Card.java
@@ -1,5 +1,25 @@
-/**
- * Created by kptox on 11.10.2017.
- */
-public class Card {
+
+
+public class Card implements Comparable<Card>{
+
+    Ranks rank;
+    Suits suit;
+
+    public Card (Suits suit, Ranks rank) {
+        this.suit = suit;
+        this.rank = rank;
+    }
+
+    public String toString() {
+        return rank.name() + " OF " + suit.name();
+    }
+
+    @Override
+    public int compareTo(Card o) {
+        if (this.rank.equals(o.rank))
+            return this.suit.compareTo(o.suit);
+        else
+            return this.rank.compareTo(o.rank);
+
+    }
 }
--- src/Deck.java
+++ src/Deck.java
@@ -1,5 +1,63 @@
-/**
- * Created by kptox on 11.10.2017.
- */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+enum Suits {
+    SPADES,
+    HEARTS,
+    DIAMONDS,
+    CLUBS;
+}
+
+enum Ranks {
+    KING, QUEEN, JACK, TEN,
+    NINE, EIGHT, SEVEN ,SIX,
+    FIVE, FOUR, THREE, TWO,
+    ACE;
+}
+
 public class Deck {
+
+    List<Card> cards;
+
+    public Deck(int amount) {
+        cards = new ArrayList<Card>();
+
+        for (Ranks rank : Ranks.values()) {
+            for (Suits suit : Suits.values()) {
+                if (cards.size() >= amount)
+                    break;
+                cards.add(new Card (suit, rank));
+            }
+        }
+        showDeck();
+        shuffleDeck();
+        showDeck();
+        sortDeck();
+        showDeck();
+    }
+
+    public void shuffleDeck() {
+        Collections.shuffle(cards);
+    }
+
+    public void sortDeck() {
+        Collections.sort(cards);
+    }
+
+    public void showDeck() {
+        for (Card card : cards) {
+            System.out.println (card.toString());
+        }
+        System.out.println();
+    }
+
+    public Card showTopCard() {
+        return cards.get(cards.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        Deck deck = new Deck(52);
+    }
+
 }
